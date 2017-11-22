747bd6ab1fddf311f3aaa04f60917b5aa6f1e822
pwong00710 <pwong00710@gmail.com>
2017-11-23 00:56:43 +0800

    updates

--- WebContent/resources/vuejs/src/module/MyCRUD.js
+++ WebContent/resources/vuejs/src/module/MyCRUD.js
@@ -6,10 +6,16 @@ var simpleGrid = Vue.component('simple-grid', {
 		return {
 			mode: 0,
 			item: {},
-			title: ''
+			title: '',
+			keyColumn: ''
 		}
 	},
     props: ['dataList', 'columns', 'searchKey'],
+	created: function () {
+		this.keyColumn = this.getKeyColumn(this.columns);
+		bus.$on('create-item', this.createItem)
+		bus.$on('update-item', this.updateItem)
+	},
     computed: {
 	  filteredUsers: function () {
 	    var self = this
@@ -26,15 +32,81 @@ var simpleGrid = Vue.component('simple-grid', {
             //this.$broadcast('showDialog', true)
             bus.$emit('showDialog', true)
         },
+		openEditItemDialog: function(key) {			
+			var currentItem = this.findItemByKey(key)
+			this.title = 'Edit Item - ' + key
+			this.mode = 2
+			this.item = {}
+			this.item = this.initItemForUpdate(currentItem)
+			//this.$broadcast('showDialog', true)
+			bus.$emit('showDialog', true)
+		},        
+        initItemForUpdate: function(p) {
+            var c = c || {};
+            for (var i in p) {
+                if (p.hasOwnProperty(i)) {
+                    if (typeof p[i] === 'object') {
+                        c[i] = Array.isArray(p[i]) ? [] : {}
+                        deepCopy(p[i], c[i])
+                    } else {
+                        c[i] = p[i]
+                    }
+                }
+            }
+            return c;
+        },
+        findItemByKey: function(key){
+            var keyColumn = this.keyColumn
+            for(var i = 0; i < this.dataList.length; i++){
+                if(this.dataList[i][keyColumn] === key){
+                    return this.dataList[i]
+                }
+            }
+        },        
         createItem: function() {
-            this.dataList.push(this.item)
-            //this.$broadcast('showDialog', false)
-            bus.$emit('showDialog', false)
-            this.item = {}
+        	if (!this.itemExists(this.keyColumn)) {
+	            this.dataList.push(this.item)
+	            //this.$broadcast('showDialog', false)
+	            bus.$emit('showDialog', false)
+	            this.item = {}
+        	} else {
+                alert(this.keyColumn + ' "' + this.item[this.keyColumn] + '" is already exists')
+            }
         },    	
         deleteItem: function(index) {
             this.dataList.splice(index, 1);
         },
+        updateItem: function() {
+            var keyColumn = this.keyColumn
+
+            for (var i = 0; i < this.dataList.length; i++) {
+                if (this.dataList[i][keyColumn] === this.item[keyColumn]) {
+                    for (var j in this.item) {
+                        this.dataList[i][j] = this.item[j]
+                    }
+                    break;
+                }
+            }
+            //this.$broadcast('showDialog', false)
+            bus.$emit('showDialog', false)
+            this.item = {}
+        },
+        itemExists: function(keyColumn) {
+            for (var i = 0; i < this.dataList.length; i++) {
+                if (this.item[keyColumn] === this.dataList[i][keyColumn])
+                    return true;
+            }
+            return false;
+        },
+        getKeyColumn: function(columns) {
+        	var keyColumn
+        	columns.forEach((val) => {
+        		if (val.isKey == true) {
+        			keyColumn = val.name
+        		}
+        	})
+        	return keyColumn
+        }
     },
     filters: {
 	  capitalize: function (value) {
@@ -67,7 +139,10 @@ var simpleGrid = Vue.component('simple-grid', {
 	                    if (this.mode === 1){
 	                        //this.$dispatch('create-item')
 	                    	bus.$emit('create-item')
-	                    }
+			            } else if(this.mode === 2){
+			                //this.$dispatch('update-item')
+			            	bus.$emit('update-item')
+			            }	                    
 	            }
 			}
 		}
